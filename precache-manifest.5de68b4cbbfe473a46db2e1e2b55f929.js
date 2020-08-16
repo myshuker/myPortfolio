d945a592877c608670460311d1028070a5aa0326
myshuker <myshuker@gmai.com>
2020-08-16 15:34:31 +0200

    Updates

---
+++ precache-manifest.5de68b4cbbfe473a46db2e1e2b55f929.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967162f0a5939624cd54170d6ff388e4",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "223614331086791e694f",
+    "url": "/myPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a46ae3fdda8806a8493e",
+    "url": "/myPortfolio/static/css/main.028d11b5.chunk.css"
+  },
+  {
+    "revision": "223614331086791e694f",
+    "url": "/myPortfolio/static/js/2.a231426f.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myPortfolio/static/js/2.a231426f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46ae3fdda8806a8493e",
+    "url": "/myPortfolio/static/js/main.599f17bd.chunk.js"
+  },
+  {
+    "revision": "ea8920835b5d4d3e2d1a",
+    "url": "/myPortfolio/static/js/runtime-main.903f7733.js"
+  },
+  {
+    "revision": "47277415d30f552420bcf2c321dc9d99",
+    "url": "/myPortfolio/static/media/QR.47277415.png"
+  },
+  {
+    "revision": "e1894e96c8a1c0390619bbe53985b64b",
+    "url": "/myPortfolio/static/media/Resume.e1894e96.pdf"
+  },
+  {
+    "revision": "edc86653f2b8e8cd25a4d68f2ac204ef",
+    "url": "/myPortfolio/static/media/my_img.edc86653.jpg"
+  }
+]);
